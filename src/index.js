449c6c9ef5d02f0b052601eb8171373d7ece9124
Anıl Başaran <nlbsrn@gmail.com>
2024-08-24 12:47:37 +0300

    res.statusCode = 200;

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const http = require('http')
-const server = http.createServer( (req,res)=>{
-    if((req.method === "GET") && (req.url === "/") ){
-        res.statusCode(200)
-        
-        res.closed()
+
+const server = http.createServer((req, res) => {
+    if ((req.method === "GET") && (req.url === "/")) {
+        res.statusCode = 200;
+        res.end("Hello, World!");
+    } else {
+        res.statusCode = 404;
+        res.end("Not Found");
     }
+});
 
-    server.listen(3001,()=>{
-        console.log("server is listening 3001 port")
-    })
-
-
-})
+server.listen(3001, () => {
+    console.log("Server is listening on port 3001");
+});
